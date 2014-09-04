407254731f1e2cb1e76d2a1e6f05929c957cf383
James Booth <jabooth@gmail.com>
2014-09-04 13:37:41 +0100

    improve setup.py for python 3 (exclude mock pathlib)

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import sys
 import versioneer
 
 project_name = 'hdf5able'
@@ -11,6 +12,14 @@ versioneer.versionfile_build = '{}/_version.py'.format(project_name)
 versioneer.tag_prefix = 'v'  # tags are like v1.2.0
 versioneer.parentdir_prefix = project_name + '-'  # dirname like 'menpo-v1.2.0'
 
+
+requirements = ['numpy>=1.8.2',
+                'h5py>=2.3.0']
+
+if sys.version_info.major == 2:
+    requirements.extend(['pathlib>=1.0',
+                         'mock>=1.0.1'])
+
 setup(name=project_name,
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
@@ -18,6 +27,4 @@ setup(name=project_name,
       author='James Booth',
       author_email='james.booth08@imperial.ac.uk',
       packages=find_packages(),
-      install_requires=['numpy>=1.8.2',
-                        'h5py>=2.3.0',
-                        'pathlib>=1.0'])
+      install_requires=requirements)
