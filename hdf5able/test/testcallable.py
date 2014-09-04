f224c9344763fd5e1dff63df86b6d5ee9492b301
James Booth <jabooth@gmail.com>
2014-09-04 13:29:34 +0100

    save/load test for function

--- hdf5able/test/testcallable.py
+++ hdf5able/test/testcallable.py
@@ -1,4 +1,8 @@
+import tempfile
 from nose.tools import raises
+from hdf5able import SerializableCallable, save, load
+
+path = tempfile.mkstemp()[1]
 
 
 def test_serialize_callable_and_test_basic():
@@ -16,8 +20,8 @@ def test_get_source():
     def a_mock_function(x, y):
         pass
 
-    sc = """def a_mock_function(x, y):\n    pass"""
-    assert sc == extract_source(a_mock_function)
+    source = """def a_mock_function(x, y):\n    pass"""
+    assert source == extract_source(a_mock_function)
 
 
 def test_use_allowed_global():
@@ -50,3 +54,26 @@ def test_use_unallowed_global_raises_was_present():
         return product(args)
 
     serialize_callable_and_test(a_mock_function, [])
+
+
+def test_save_serializable_callable():
+    from itertools import product
+    import itertools
+
+    def a_mock_function(*args):
+        return product(args)
+
+    sc = SerializableCallable(a_mock_function, [itertools])
+    save(path, sc)
+
+
+def test_load_serializable_callable():
+
+    def a_function(*args):
+        import numpy as np
+        return np.sum(args)
+
+    sc = SerializableCallable(a_function, [])
+    save(path, sc)
+    f = load(path)
+    assert a_function(2, 4) == f(2, 4)
